d0b2dbc0df81d892792c12191406fdb025aa03cc
letsrain <66619884+letsrain@users.noreply.github.com>
2021-05-23 19:44:43 +0200

    Add files via upload

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -32,7 +32,6 @@ import java.io.InputStreamReader;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
-@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -40,9 +39,9 @@ public class DemoApplication {
 
     ViberBot viberBot = new ViberBot("My Bot", "<AUTH_TOKEN>");
     // start server on 8080 port with path /callback
-//         viberBot.listen("/");
-//     // let Viber API know about webhook url
-//         viberBot.setWebhook("https://test-vbbot.herokuapp.com/");
+//        viberBot.listen("/");
+    // let Viber API know about webhook url
+//        viberBot.setWebhook("https://test-vbbot.herokuapp.com/");
     // subscribes on message events
         viberBot.addMessageListener((event, response) -> {
         Message message = event.getMessage();
